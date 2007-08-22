3c4de7b07479f93967c751ca3b2125c9fd921ad8
Mathias Ertl <mati@fsinf.at>
2007-08-22 22:16:39 +0000

    no_page_specified now also has a header

--- Resources.i18n.php
+++ Resources.i18n.php
@@ -3,7 +3,8 @@ $allMessages = array(
         'en' => array(
 		'resources_title' => 'Resources',
 		'resources' => 'Resources',
-		'no_page_specified' => 'Error: No pagename was specified',
+		'no_page_specified' => '= Special:Resources =
+Error: No pagename was specified',
 		'header' => '= Resources for $1 =',
 		'header_text' => 'There are currently $1 resources for this page. You may want to [[Special:AddResource/$2|add a new one]]?',
 		'header_text_none' => 'There are currently \'\'\'no resources\'\'\' for this page. You can of course [[Special:AddResource/$1|add a new one]]!',
@@ -12,7 +13,8 @@ $allMessages = array(
 	'de' => array(
 		'resources_title' => 'Materialien',
 		'materialien' => 'Materialien',
-		'no_page_specified' => 'Fehler: Kein Artikel angegeben.',
+		'no_page_specified' => '= Spezial:Materialien =
+Fehler: Kein Artikel angegeben.',
 		'header' => '= Materialien für $1 =',
 		'header_text' => 'Es gibt derzeit $1 Materialien für diese Seite. Du kannst auch einen [[Spezial:Materialien hinzufügen|neuen hinzufügen]]!',
 		'header_text_none' => 'Es gibt derzeit noch \'\'\'keine Materialien\'\'\' für diese Seite. Du kannst aber [[Spezial:Materialien hinzufügen|neue Materialien hinzufügen]].',
