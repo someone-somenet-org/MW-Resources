50a7c8e783392b2b111a6d81941734d71c737e09
Mathias Ertl <mati@fsinf.at>
2007-08-22 20:53:40 +0000

    updated translations...

--- Resources.i18n.php
+++ Resources.i18n.php
@@ -7,7 +7,7 @@ $allMessages = array(
 		'header' => '= Resources for $1 =',
 		'header_text' => 'There are currently $1 resources for this page. You may want to [[Special:AddResource/$2|add a new one]]?',
 		'header_text_none' => 'There are currently \'\'\'no resources\'\'\' for this page. You can of course [[Special:AddResource/$1|add a new one]]!',
-		'redirect_link_view' => 'view',
+		'redirect_link_view' => 'edit link',
 	),
 	'de' => array(
 		'downloads_title' => 'Downloads',
