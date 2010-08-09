3023bc41191df488068cbcb18e2109336aac7de1
Mathias Ertl <mati@fsinf.at>
2010-08-09 21:36:08 +0000

    Resources: bump to version 1.5 for some limited functionality on the "Vector" theme.

--- Resources.php
+++ Resources.php
@@ -15,12 +15,105 @@ $wgExtensionMessagesFiles['Resources'] = $dir . '/Resources.i18n.php';
 $wgSpecialPages[ 'Resources' ] = 'Resources';
 $wgHooks['LanguageGetSpecialPageAliases'][] = 'efResourcesLocalizedPageName';
 
-$wgHooks['SkinTemplateContentActions'][] = 'efResourcesDisplayTab';
+$wgHooks['SkinTemplateContentActions'][] = 'efResourcesDisplayTab'; # for monobook
+$wgHooks['SkinTemplateNavigation'][] = 'efResourcesNormalPages'; # for vector - just on normal pages
+
+/**
+ * this function is not currently used and is only here for future reference
+ */
+function efResourcesSpecialPage( $template, $links ) {
+	global $wgTitle;
+
+	/* we are on a special page */
+	$curSpecialPage = $wgTitle->getPrefixedText();
+
+	// return if we are not on the right special page
+	if ( $curSpecialPage != SpecialPage::getTitleFor( 'Resources' ) )
+		return true;
+
+	// get parameter for special page:
+	global $wgRequest, $wgUser, $wgAddResourceTab;
+	$reqTitle = $wgRequest->getVal('title');
+	$par = preg_replace('/' . $curSpecialPage . '\/?/', '', $reqTitle );
+	if ( $par == '' ) // if no /par was given
+		return true;
+	$parTitle = Title::newFromText( $par )->getSubjectPage();
+	$parTalkTitle = $parTitle->getTalkPage();
+
+	$head = array (
+		$parTitle->getNamespaceKey('') => array(
+			'class' => $parTitle->exists() ? null : 'new',
+			'text' => $parTitle->getNsText(),
+			'href' => '/href', // todo
+		)
+	);
+	$tail = array (
+		$parTitle->getNamespaceKey('') . '_talk' => array(
+			'class' => $parTalkTitle->exists() ? null : 'new',
+			'text' => $parTalkTitle->getNsText(),
+			'href' => '/href', // todo
+		)
+	);
+	$links = array_merge( $head, $links, $tail );
+
+	return true;
+}
+
+/**
+ * Add Resource Tab on normal pages on SkinTemplates (like Vector). This does
+ * not handle special pages, for which unfortunatly no hook is called (also see
+ * the above function).
+ */
+function efResourcesNormalPages( $template, $links ) {
+	global $wgResourcesNamespaces, $wgResourcesTabs, $wgTitle;
+	if ( ! $wgResourcesTabs ) 
+		return true;
+	$ns = $wgTitle->getNamespace();
+	
+	if ( ! ( in_array( $ns, $wgResourcesNamespaces ) ||
+			in_array( $ns - 1, $wgResourcesNamespaces ) ) )
+		return true; /* admin doesn't want tab here */
+
+	# get class for resources tab:
+	$resourcePage = new Resources();
+	$resourceCount = $resourcePage->getResourceListCount( $wgTitle );
+	$class = $resourceCount > 0 ? null : 'new';
+
+	# get link target:
+	$resources = SpecialPage::getTitleFor( 'Resources' );
+	$target = $resources->getLocalURL() .'/'. $wgTitle->getPrefixedDBkey();
+
+	# resource tab text:
+	if ( $resourceCount > 0 ) {
+		$text = wfMsg( 'resourcesTabExists', $resourceCount );
+	} else {
+		$text = wfMsg( 'resourcesTab' );
+	}
+	
+	$namespaces = $links['namespaces'];
+	$namespace_key = array_keys( $namespaces );
+
+	$resourcesTab = array( 
+		$namespace_key[0] . '_resources' => array(
+			'class' => $class,
+			'text' => $text,
+			'href' => $target,
+			'context' => 'resources',
+		)
+	);
+	
+	# build array:
+	$head = array_slice( $namespaces, 0, 1 );
+	$tail = array_slice( $namespaces, 1 );
+	$links['namespaces'] = array_merge( $head, $resourcesTab, $tail );
+
+	return true;
+}
 
 $wgExtensionCredits['specialpage'][] = array (
 	'name' => 'Resources',
 	'description' => 'Displays resources attached to an article (with the AddResource extension)',
-	'version' => '1.4.1-1.13.1',
+	'version' => '1.5.0-1.13.1',
 	'author' => 'Mathias Ertl',
 	'url' => 'http://pluto.htu.tuwien.ac.at/devel_wiki/Resources',
 );
@@ -39,6 +132,9 @@ function efResourcesLocalizedPageName( &$specialPageArray, $code) {
 	return true;
 }
 
+/**
+ * Add Tabs for Skins like monobook
+ */
 function efResourcesDisplayTab( $tabs ) {
 	global $wgResourcesTabs, $wgTitle;
 	if ( ! $wgResourcesTabs ) 
@@ -74,7 +170,7 @@ function efResourcesDisplayTab( $tabs ) {
 
 		// downloads-tab:
 		$customTabs['view-resources'] = array ( 'class' => 'selected',
-			'text' => wfMsg('ResourcesTab'),
+			'text' => wfMsg('resourcesTab'),
 			$tabs['nstab-special']['href'] );
 		
 		/* get number of resources (and redden link if 0) */
@@ -119,18 +215,25 @@ function efResourcesDisplayTab( $tabs ) {
 		if ( $titleBase->exists() )
 			$title = $titleBase;
 		
+		/* get number of resources (and redden link if 0) */
+		$resourcePage = new Resources();
+		$resourceCount = $resourcePage->getResourceListCount( $title );
+		if ( $resourceCount > 0 ) {
+			$tabText = wfMsg( 'resourcesTabExists', $resourceCount );
+			$class = null;
+		} else {
+			$tabText = wfMsg( 'resourcesTab' );
+			$class = 'new';
+		}
+		
 		$specialPage = SpecialPage::getTitleFor( 'Resources' );
 		$mainTabs['view-resources'] = array( 'class' => false,
-			'text' => wfMsg( 'resourcesTab' ),
+			'text' => $tabText,
 			'href' => $specialPage->getLocalURL() . '/' .
-				$title->getPrefixedDBkey() );
+				$title->getPrefixedDBkey(),
+			'class' => $class,
+		);
 		
-		/* get number of resources (and redden link if 0) */
-		$resourcesPage = new Resources();
-		$resourcesCount = $resourcesPage->getResourceListCount( $title );
-		if ( ! $resourcesCount )
-			$mainTabs['view-resources']['class'] = 'new';
-
 		$tabs = array_merge( $mainTabs, $tabs );
 	}
 
