ff445e66f5ad31723fc355c50a8ad816471fbf19
Mathias Ertl <mati@fsinf.at>
2013-05-26 14:17:58 +0200

    add aliases file

---
+++ Resources.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for Resources
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Mathias Ertl
+ */
+$specialPageAliases['en'] = array(
+        'Resources' => array('Resources', 'Resources'),
+);
+
+/** Deutsch
+ * @author Mathias Ertl
+ */
+$specialPageAliases[ 'de' ] = array(
+        'Resources' => array( 'Materialien', 'Materialien' ),
+);
